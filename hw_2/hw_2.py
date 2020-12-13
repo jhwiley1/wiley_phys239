5579ace927ef76eadab38b687da2eb89ddf7a440
jhwiley1 <jhwiley@ucsd.edu>
2020-12-13 01:35:16 -0800

    E

--- hw_2/hw_2.py
+++ hw_2/hw_2.py
@@ -16,7 +16,7 @@ def function2(D,n,cro_sec,I_0,S):
     #Fucntion for problem 2, cro_sec is a single value
 
     step_arr=np.linspace(0,D*3.086*10.**18,1000) #Define array 0 to D with stepsize ds
-    int_arr=np.zeros((len(step_arr),1)) #Initialize another array using same steps but holding intensity values
+    int_arr=np.zeros((len(step_arr),)) #Initialize another array using same steps but holding intensity values
     int_arr[0]=I_0 #set first element to I_0
     opt_dep=cro_sec*float(D)*float(n)*3.086*10.**18
     opt_deps=np.linspace(0,opt_dep,1000)
@@ -24,36 +24,39 @@ def function2(D,n,cro_sec,I_0,S):
     
     for i in range(0,len(int_arr)):
     
-        temp=temp+S*math.e**(-opt_deps[i])*opt_dep/1000
-    
+        temp=temp+S*(math.e**(-opt_dep+opt_deps[i]))*opt_dep/1000
+
         if i != 0:
-            int_arr[i]=I_0*math.e**(-opt_deps[i])+temp
+            int_arr[i]=I_0*(math.e**(-opt_deps[i]))+temp
     
     return(float(int_arr[len(int_arr)-1]))
 
-def function3(v,cro_sec_0):
+def function3(v,cro_sec_0,center,width):
     #Function for problem 3.  returns an array of cross sections in the shape of a gaussian given an input array of frequencies.
 
-    gauss=cro_sec_0*math.e**(-np.square(v-500)/500)
+    gauss=cro_sec_0*math.e**(-np.square(v-center)/width**2)
     print(len(gauss))
     return gauss
 
-def function4(cro_secs,I_0,S):
+def function4(v,cro_secs,I_0,S):
     #Function for problem 4. Plots intensity versus frequency given conditions
     #Cro_secs is an array of cross sections
     #I_0 is initial intensity
     #S is source function
     #T is optical depth
 
-    D=100
-    n=1
+    D=100.
+    n=1.
     I_D=np.zeros((1000,))
     
     for i in range(0,len(cro_secs)):
-
+    
         I_D[i]=function2(D,n,cro_secs[i],I_0,S)
 
-    plt.plot(np.linspace(1,1000,1000),I_D)
+    plt.plot(v,I_D)
+    plt.xlabel('Frequency')
+    plt.ylabel('Intensity')
+    plt.title('Observed Intensity from cloud')
     plt.show()
 
 ###########################################################################################################################################################
@@ -87,11 +90,13 @@ print('Problem 3:')
 
 print('The function has been made!')
 
-v=np.linspace(1,1000,1000)
+v=np.linspace(10**-7,10**-6,1000)
 cro_secs=[[]]*4
+center=5*10**-7
+width=2.5*10**-8
 for i in range(0,3):
-
-    cro_secs[i]=function3(v,cro_sec[i])
+    
+    cro_secs[i]=function3(v,cro_sec[i],center,width)
     plt.plot(v,cro_secs[i])
     plt.xlabel('frequency')
     plt.ylabel('cross section (cm^-2)')
@@ -107,21 +112,21 @@ print('(a)')
 I_0=0
 S=1
 
-function4(cro_secs[0],I_0,S) #cro_secs[0] gives cross sections for optical depth of 10^-3
+function4(v,cro_secs[0],I_0,S) #cro_secs[0] gives cross sections for optical depth of 10^-3
 
 print('(b)')
 
 I_0=10
 S=1
 
-function4(cro_secs[0],I_0,S)
+function4(v,cro_secs[0],I_0,S)
 
 print('(c)')
 
 I_0=1
 S=10
 
-function4(cro_secs[0],I_0,S)
+function4(v,cro_secs[0],I_0,S)
 
 print('(d)')
 
@@ -129,18 +134,19 @@ I_0=1
 S=1
 cro_secs[3]=np.ones((1000,))
 
-function4(cro_secs[3],I_0,S)
+function4(v,cro_secs[3],I_0,S)
 
 print('(e)')
 
 I_0=1
-S=10
+S=5
 
-function4(cro_secs[1],I_0,S)
+function4(v,cro_secs[2],I_0,S)
 
 print('(f)')
 
-I_0=10
+I_0=5
 S=1
-function4(cro_secs[1],I_0,S)
+
+function4(v,cro_secs[2],I_0,S)
 
